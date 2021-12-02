f2bf95c8dd2a4a8e99697efd42863a4c1a468b52
Highfire1 <tseng.andersonn@gmail.com>
2021-12-02 01:44:15 -0800

    day 1

---
+++ day 1/1.py
@@ -0,0 +1,12 @@
+count = 0
+
+# part 1
+with open("day 1/in.txt", "r") as measurements:
+    previtem = measurements.readline()
+
+    for item in measurements.readlines():
+        if item > previtem:
+            count += 1
+        previtem = item
+
+print(count + 1) # off by one, not sure why ;-;
